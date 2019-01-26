30513e699e5752f0fd5400ff45ba6284aa7460bf
yazanmaini <mainiyazan@gmail.com>
2019-01-26 00:38:00 -0800

    java script

---
+++ code.js
@@ -0,0 +1,34 @@
+// delete and completed svg icons
+var deleteSVG = '<svg viewBox="0 0 20 20"><path class="fill" d="M7.083,8.25H5.917v7h1.167V8.25z M18.75,3h-5.834V1.25c0-0.323-0.262-0.583-0.582-0.583H7.667c-0.322,0-0.583,0.261-0.583,0.583V3H1.25C0.928,3,0.667,3.261,0.667,3.583c0,0.323,0.261,0.583,0.583,0.583h1.167v14c0,0.644,0.522,1.166,1.167,1.166h12.833c0.645,0,1.168-0.522,1.168-1.166v-14h1.166c0.322,0,0.584-0.261,0.584-0.583C19.334,3.261,19.072,3,18.75,3z M8.25,1.833h3.5V3h-3.5V1.833z M16.416,17.584c0,0.322-0.262,0.583-0.582,0.583H4.167c-0.322,0-0.583-0.261-0.583-0.583V4.167h12.833V17.584z M14.084,8.25h-1.168v7h1.168V8.25z M10.583,7.083H9.417v8.167h1.167V7.083z"></path></svg>';
+
+var completeSVG = '<svg viewBox="0 0 20 20"> <path class="fill" d="M10.219,1.688c-4.471,0-8.094,3.623-8.094,8.094s3.623,8.094,8.094,8.094s8.094-3.623,8.094-8.094S14.689,1.688,10.219,1.688 M10.219,17.022c-3.994,0-7.242-3.247-7.242-7.241c0-3.994,3.248-7.242,7.242-7.242c3.994,0,7.241,3.248,7.241,7.242C17.46,13.775,14.213,17.022,10.219,17.022 M15.099,7.03c-0.167-0.167-0.438-0.167-0.604,0.002L9.062,12.48l-2.269-2.277c-0.166-0.167-0.437-0.167-0.603,0c-0.166,0.166-0.168,0.437-0.002,0.603l2.573,2.578c0.079,0.08,0.188,0.125,0.3,0.125s0.222-0.045,0.303-0.125l5.736-5.751C15.268,7.466,15.265,7.196,15.099,7.03"></path> </svg>';
+
+// if user clicks on the add button
+document.getElementById('add').addEventListner('click', function() {
+  var value = document.getElementById('item').value;
+  if  (value) addItem(value);
+});
+
+
+function addItem(text) {
+
+    var list = document.getElementById('it');
+    var item = document.createElement('li');
+    item.innerText = text;
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    var deleted = document.createElement('button');
+    deleted.classList.add('delete');
+    deleted.innerHTML = deleteSVG;
+    var completed = document.createElement('button');
+    completed.classList.add('completed');
+    completed.innerHTML = completeSVG;
+
+    buttons.appendChild(deleted);
+    buttons.appendChild(completed);
+    item.appendChild(buttons)
+    list.appendChild(item);
+
+}
